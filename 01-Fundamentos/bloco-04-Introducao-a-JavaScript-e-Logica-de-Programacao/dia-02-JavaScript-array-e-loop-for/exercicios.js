7766706770c9710bc7a56b74f33bddda794af41c
Arthur <martinsarthur07@gmail.com>
2022-02-03 19:18:25 -0300

    exercicios 3 e 4

--- 01-Fundamentos/bloco-04-Introducao-a-JavaScript-e-Logica-de-Programacao/dia-02-JavaScript-array-e-loop-for/exercicios.js
+++ 01-Fundamentos/bloco-04-Introducao-a-JavaScript-e-Logica-de-Programacao/dia-02-JavaScript-array-e-loop-for/exercicios.js
@@ -1,14 +1,40 @@
 // 1 ao 7.
 
+// 1.
 /* let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 for (let i = 0; i < numbers.length; i += 1) {
     console.log(numbers[i])
 } */
 
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// 2.
+/* let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let soma = 0
 for (let i = 0; i < numbers.length; i += 1) {
     soma = numbers[i] + soma
 }
 
-console.log(soma);
+console.log(soma); */
+
+// 3.
+/* let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0
+for (let i = 0; i < numbers.length; i += 1) {
+    soma = numbers[i] + soma
+}
+soma = soma / numbers.length
+
+console.log(soma); */
+
+// 4.
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0
+for (let i = 0; i < numbers.length; i += 1) {
+    soma = numbers[i] + soma
+}
+soma = soma / numbers.length
+
+if (soma > 20) {
+    console.log("valor maior que 20");
+} else {
+    console.log("valor menor ou igual a 20");
+}
