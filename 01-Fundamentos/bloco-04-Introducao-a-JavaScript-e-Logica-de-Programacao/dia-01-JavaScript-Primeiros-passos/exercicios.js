de8b54059753fa89b1e6fa265c2b6e366a72cac3
Arthur <martinsarthur07@gmail.com>
2022-02-03 11:21:51 -0300

    exercicios 1, 2, 3 e 4

---
+++ 01-Fundamentos/bloco-04-Introducao-a-JavaScript-e-Logica-de-Programacao/dia-01-JavaScript-Primeiros-passos/exercicios.js
@@ -0,0 +1,45 @@
+// 1.
+/* 
+const a = 5;
+const b = 2;
+
+console.log(a + b)
+console.log(a - b)
+console.log(a * b)
+console.log(a / b)
+console.log(a % b) */
+
+// 2.
+
+/* const numb1 = 7;
+const numb2 = 8;
+
+if(numb1 > numb2) {
+    console.log(numb1);
+}else {
+    console.log(numb2);
+}*/
+
+// 3.
+
+/* const numb1 = 8;
+const numb2 = 9;
+const numb3 = 7;
+
+if(numb1 > numb2 && numb1 > numb3){
+    console.log(numb1);
+} else if(numb2 > numb1 && numb2 > numb3) {
+    console.log(numb2);
+} else {
+    console.log(numb3)
+}*/
+
+// 4.
+
+const numb1 = 6
+
+if (numb1 > 0){
+    console.log("positive")
+} else {
+    console.log("negative")
+}
