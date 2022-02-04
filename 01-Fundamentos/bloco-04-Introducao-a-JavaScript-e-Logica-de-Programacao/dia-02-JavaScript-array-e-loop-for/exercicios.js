467455f12d9a7f400ed0e18631a3b99e0a7d59be
Arthur <martinsarthur07@gmail.com>
2022-02-04 11:32:03 -0300

    exercicio 7

--- 01-Fundamentos/bloco-04-Introducao-a-JavaScript-e-Logica-de-Programacao/dia-02-JavaScript-array-e-loop-for/exercicios.js
+++ 01-Fundamentos/bloco-04-Introducao-a-JavaScript-e-Logica-de-Programacao/dia-02-JavaScript-array-e-loop-for/exercicios.js
@@ -62,3 +62,11 @@ for (let i = 0; i < numbers.length; i += 1){
 } */
 
 // 7.
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let menorNumero = 101;
+for (let i = 0; i < numbers.length; i += 1) {
+    if (menorNumero > numbers[i]) {
+        menorNumero = numbers[i]
+    }
+}
+console.log(menorNumero)
