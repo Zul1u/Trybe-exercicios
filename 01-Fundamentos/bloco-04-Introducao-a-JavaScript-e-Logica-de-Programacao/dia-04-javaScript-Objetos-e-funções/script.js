40a8944f886c8bf5532a6b981373c77dcf624856
Arthur <martinsarthur07@gmail.com>
2022-02-07 16:07:28 -0300

    para fixar 2

--- 01-Fundamentos/bloco-04-Introducao-a-JavaScript-e-Logica-de-Programacao/dia-04-javaScript-Objetos-e-funções/script.js
+++ 01-Fundamentos/bloco-04-Introducao-a-JavaScript-e-Logica-de-Programacao/dia-04-javaScript-Objetos-e-funções/script.js
@@ -1,4 +1,5 @@
-let player = {
+// Para fixar 1.
+/* let player = {
     name: 'Marta',
     lastName: 'Silva',
     age: 34,
@@ -10,8 +11,29 @@ let player = {
 
 console.log("A jogadora " + player.name + " " + player.lastName + " tem " + player.age + " anos de idade.")
 
-//player['bestInTheWorld'] = [2006, 2007, 2008, 2009, 2010, 2018]
+player['bestInTheWorld'] = [2006, 2007, 2008, 2009, 2010, 2018]
 
-console.log('A jogadora ' + player['name'] + ' ' + player['lastName'] + ' foi eleita a melhor do mundo por ' + player['bestInTheWorld'.length - 1] + ' vezes'  )
+console.log('A jogadora ' + player.name + ' ' + player.lastName + ' foi eleita a melhor do mundo por ' + player.bestInTheWorld.length + ' vezes'  )
 
-console.log('A jogadora possui ' + player.medals.golden  + ' medalhas de ouro e ' + player.medals.silver + ' medalhas de prata');
+console.log('A jogadora possui ' + player.medals.golden  + ' medalhas de ouro e ' + player.medals.silver + ' medalhas de prata'); */
+
+// Para fixar 2.
+let names = {
+    person1: 'João',
+    person2: 'Maria',
+    person3: 'Jorge',
+};
+
+for (let pessoa in names) {
+    console.log('Olá ' + names[pessoa])
+}
+
+let car = {
+    model: 'A3 Sedan',
+    manufacturer: 'Audi',
+    year: 2020
+};
+
+for (let key in car) {
+    console.log(key, car[key])
+}
