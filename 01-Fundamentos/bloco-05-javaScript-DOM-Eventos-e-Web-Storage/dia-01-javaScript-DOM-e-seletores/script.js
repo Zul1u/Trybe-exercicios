94a4ccdacbe22ba8a8fc6646a37e7436ae0b9128
Arthur <martinsarthur07@gmail.com>
2022-02-09 19:02:51 -0300

    Para Fixar 2

---
+++ 01-Fundamentos/bloco-05-javaScript-DOM-Eventos-e-Web-Storage/dia-01-javaScript-DOM-e-seletores/script.js
@@ -0,0 +1,13 @@
+document.getElementsByTagName('header')[0].style.backgroundColor = "green";
+
+document.querySelectorAll(".emergency-tasks")[0].style.backgroundColor = "orange";
+
+document.getElementsByTagName('h3')[0].style.backgroundColor = 'purple';
+document.getElementsByTagName('h3')[1].style.backgroundColor = 'purple';
+
+document.querySelectorAll(".no-emergency-tasks")[0].style.backgroundColor = 'yellow'
+
+document.getElementsByTagName('h3')[2].style.backgroundColor = 'black';
+document.getElementsByTagName('h3')[3].style.backgroundColor = 'black';
+
+document.querySelector('footer').style.backgroundColor = "darkgreen"
