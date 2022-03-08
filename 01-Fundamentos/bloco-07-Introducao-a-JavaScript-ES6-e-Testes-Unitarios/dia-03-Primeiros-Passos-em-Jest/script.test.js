cc7a5b2aea3a8cc0d06e9c24e7a636b7febb5d9f
Arthur <martinsarthur07@gmail.com>
2022-03-08 14:04:30 -0300

    exercicios 7.3 requisito 2

--- 01-Fundamentos/bloco-07-Introducao-a-JavaScript-ES6-e-Testes-Unitarios/dia-03-Primeiros-Passos-em-Jest/script.test.js
+++ 01-Fundamentos/bloco-07-Introducao-a-JavaScript-ES6-e-Testes-Unitarios/dia-03-Primeiros-Passos-em-Jest/script.test.js
@@ -1,4 +1,4 @@
-const { sum } = require('./script');
+const {sum, myRemove} = require('./script');
 
 describe('Fução sum', () => {
 
@@ -7,13 +7,34 @@ describe('Fução sum', () => {
   });
 
   it('A função sum deve retornar que a soma de 0 e 0 é igual a 0', () => {
-      expect(sum(0, 0)).toBe(0);
+    expect(sum(0, 0)).toBe(0);
   });
 
   it('A função sum deve retornar um erro quando tentar somar 4 e "5"', () => {
-      expect(() => sum(4, '5')).toThrowError();
+    expect(() => sum(4, '5')).toThrowError();
   });
-  it('A mensagem de erro deve ser "parameters must be numbers"', () =>{
-      expect(() => sum(4, '5')).toThrowError('parameters must be numbers');
+
+  it('A mensagem de erro deve ser "parameters must be numbers"', () => {
+    expect(() => sum(4, '5')).toThrowError('parameters must be numbers');
   });
+
+});
+
+describe('Função myRemove', () =>{
+
+  it('A função myRemove deve retornar [1, 2, 4]', () => {
+    const array = [1, 2, 3, 4]
+    expect(myRemove(array, 3)).toEqual([1, 2, 4]);
+  });
+
+  it('A função myRemove não deve retornar [1, 2, 3, 4]', () => {
+    const array = [1, 2, 3, 4]
+    expect(myRemove(array, 3)).not.toEqual([1, 2, 3, 4]);
+  });
+
+  it('A função myRemove deve retornar [1, 2, 4]', () => {
+    const array = [1, 2, 3, 4]
+    expect(myRemove(array, 5)).toEqual([1, 2, 3, 4]);
+  });
+
 })
