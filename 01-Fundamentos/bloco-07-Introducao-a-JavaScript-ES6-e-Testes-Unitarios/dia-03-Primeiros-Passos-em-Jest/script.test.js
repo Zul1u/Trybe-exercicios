9aae11ef933fcce8d568ce82917727172fc0695e
Arthur <martinsarthur07@gmail.com>
2022-03-08 11:43:10 -0300

    exercicios 7.3 requisito 1

---
+++ 01-Fundamentos/bloco-07-Introducao-a-JavaScript-ES6-e-Testes-Unitarios/dia-03-Primeiros-Passos-em-Jest/script.test.js
@@ -0,0 +1,19 @@
+const { sum } = require('./script');
+
+describe('Fução sum', () => {
+
+  it('A função sum deve retornar que a soma de 4 e 5 é igual a 9 ', () => {
+    expect(sum(4, 5)).toBe(9);
+  });
+
+  it('A função sum deve retornar que a soma de 0 e 0 é igual a 0', () => {
+      expect(sum(0, 0)).toBe(0);
+  });
+
+  it('A função sum deve retornar um erro quando tentar somar 4 e "5"', () => {
+      expect(() => sum(4, '5')).toThrowError();
+  });
+  it('A mensagem de erro deve ser "parameters must be numbers"', () =>{
+      expect(() => sum(4, '5')).toThrowError('parameters must be numbers');
+  });
+})
