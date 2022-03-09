962bae2b562950f203244d62bae73efb1aefe81f
Arthur <martinsarthur07@gmail.com>
2022-03-09 18:20:08 -0300

    exerciocio 8.1 para fixar

---
+++ 01-Fundamentos/bloco-08-Higher-Order-Functions-do-JavaScript-ES6/dia-01-JavaScript-ES6-introducao-a-Higher-Order-Functions/script.js
@@ -0,0 +1,9 @@
+const morning = () => 'Acordando!!';
+
+const afternoon = () => 'Bora tomar café!!';
+
+const night = () => 'Partiu dormir!!';
+
+const doingThings = (partOfTheDay) => console.log(partOfTheDay);
+
+doingThings(night());
