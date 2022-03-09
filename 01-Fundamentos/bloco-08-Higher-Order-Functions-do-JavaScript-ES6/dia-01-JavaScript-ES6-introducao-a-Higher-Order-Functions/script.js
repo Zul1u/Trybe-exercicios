0e1d6e0e2e66352306fa7ce0722fdcb2a15d2237
Arthur <martinsarthur07@gmail.com>
2022-03-09 18:49:52 -0300

    exerciocios 8.1 requisito 1

--- 01-Fundamentos/bloco-08-Higher-Order-Functions-do-JavaScript-ES6/dia-01-JavaScript-ES6-introducao-a-Higher-Order-Functions/script.js
+++ 01-Fundamentos/bloco-08-Higher-Order-Functions-do-JavaScript-ES6/dia-01-JavaScript-ES6-introducao-a-Higher-Order-Functions/script.js
@@ -1,4 +1,5 @@
-const morning = () => 'Acordando!!';
+//EXERCICIO PARA FIXAR 8.1
+/* const morning = () => 'Acordando!!';
 
 const afternoon = () => 'Bora tomar café!!';
 
@@ -6,4 +7,24 @@ const night = () => 'Partiu dormir!!';
 
 const doingThings = (partOfTheDay) => console.log(partOfTheDay);
 
-doingThings(night());
+doingThings(night()); */
+
+// EXERCICIOS 8.1 
+// 1.
+
+const person = (name)  => {
+  const email = `${name.replace(/ /g, '_').toLowerCase()}@trybe.com `
+
+  return { name, email: email };
+};
+
+const newEmployees = (callback) => {
+    const employees = {
+      id1: callback('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+      id2: callback('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+      id3: callback('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+    }
+    return employees;
+  };
+
+  console.log(newEmployees(person));
