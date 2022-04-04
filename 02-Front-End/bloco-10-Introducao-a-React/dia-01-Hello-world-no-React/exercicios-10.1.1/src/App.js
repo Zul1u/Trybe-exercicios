75434f91379632e941d72cf896b765bac2c99081
Arthur <martinsarthur07@gmail.com>
2022-04-04 19:14:29 -0300

    Exercicios 10.1 Nº 1 e 2

---
+++ 02-Front-End/bloco-10-Introducao-a-React/dia-01-Hello-world-no-React/exercicios-10.1.1/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+const Task = (value) => {
+  return (
+    <li>{value}</li>
+  );
+}
+
+const tarefas = ['Tomar café da manhã', 'Escovar os dentes', 'Ir trabalhar']
+
+function App() {
+
+  return (
+    <ul>
+      {tarefas.map((array) => Task(array))}
+    </ul>
+  );
+}
+
+export default App;
